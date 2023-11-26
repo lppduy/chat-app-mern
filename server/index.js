120f2ea0cd243aeda169d421100bd11096bc59da
lppduy <lephamphuongduy@gmail.com>
2023-11-26 14:21:48 +0700

    added chat apis

--- server/index.js
+++ server/index.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+
 const userRoute = require('./routes/userRoute');
+const chatRoute = require('./routes/chatRoute');
+
 const app = express();
 const PORT = process.env.PORT || 8080;
 require('dotenv').config();
 
 app.use(express.json());
 app.use(cors());
+
 app.use('/api/users', userRoute);
+app.use('/api/chats', chatRoute);
 
 app.get('/', (req, res) => {
   res.send('WELCOME OUR CHAT APP APIS...');
